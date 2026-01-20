2df9ba05fbade74ca0a99a73e7c9a6278c5b496d
sdcomayagua <sdcomayagua@gmail.com>
2026-01-20 14:49:31 -0600

    Create config.js

---
+++ assets/js/config.js
@@ -0,0 +1,28 @@
+window.CONFIG = {
+  storeName: "Soluciones Digitales Comayagua",
+  storeShort: "SDComayagua",
+  whatsappNumber: "50431517755",
+  couriers: {
+    "C807": 110,
+    "Cargo Expreso": 120,
+    "Forza": 100
+  },
+  comayaguaLocalZones: {
+    "Centro": 20,
+    "Zona media": 30,
+    "Periferia": 50,
+    "Juterique/Lejamaní/VillaSanAntonio/Flores": 80
+  },
+  laPazRange: { min: 100, max: 120 },
+  departamentos: [
+    "Atlántida","Choluteca","Colón","Comayagua","Copán","Cortés","El Paraíso",
+    "Francisco Morazán","Gracias a Dios","Intibucá","Islas de la Bahía","La Paz",
+    "Lempira","Ocotepeque","Olancho","Santa Bárbara","Valle","Yoro"
+  ],
+  municipiosPorDepartamento: {
+    "Comayagua": ["Comayagua","Ajuterique","El Rosario","Esquías","Humuya","La Libertad","Lamaní","La Trinidad","Lejamaní","Meámbar","Minas de Oro","Ojos de Agua","San Jerónimo","San José de Comayagua","San José del Potrero","San Luis","San Sebastián","Siguatepeque","Villa de San Antonio","Las Lajas","Taulabé"],
+    "La Paz": ["La Paz","Marcala","Santa Ana"],
+    "Francisco Morazán": ["Tegucigalpa","Valle de Ángeles","Santa Lucía"],
+    "defaultSample": ["Municipio A","Municipio B"]
+  }
+};
