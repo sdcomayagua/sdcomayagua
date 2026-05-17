213d152d381b2ff0536d8657dec5938b73f64431
sdcomayagua <sdcomayagua@gmail.com>
2026-05-17 01:43:11 -0600

    Conecta editor visual de promociones v1.4.6

--- js/assets-enhancer.js
+++ js/assets-enhancer.js
@@ -1,6 +1,6 @@
-// SD COMAYAGUA POS - cargador visual seguro v1.4.5
+// SD COMAYAGUA POS - cargador visual seguro v1.4.6
 (function () {
-  const VERSION = '1.4.5';
+  const VERSION = '1.4.6';
   const LOGO = `assets/logo-sdc.svg?v=${VERSION}`;
   const RECEIPT_LOGO = `assets/logo-sdc-receipt.svg?v=${VERSION}`;
 
@@ -14,7 +14,8 @@
     'css/catalog-shipping-v141.css',
     'css/stock-states-v143.css',
     'css/shipping-modal-polish-v144.css',
-    'css/catalog-inventory-polish-v145.css'
+    'css/catalog-inventory-polish-v145.css',
+    'css/product-promo-editor-v146.css'
   ];
   const jsFiles = [
     'js/document-actions.js',
@@ -23,7 +24,8 @@
     'js/catalog-shipping-quote.js',
     'js/shipping-polish-v142.js',
     'js/stock-states-v143.js',
-    'js/catalog-admin-tools-v145.js'
+    'js/catalog-admin-tools-v145.js',
+    'js/product-promo-editor-v146.js'
   ];
 
   function loadCss(path) {
