b49ebec9327b08fce266c9770401ebe00a401194
sdcomayagua <sdcomayagua@gmail.com>
2026-07-19 12:27:57 -0600

    Renueva caché para productos agotados

--- assets/js/promotions.js
+++ assets/js/promotions.js
@@ -2,7 +2,7 @@
 // La lógica principal permanece en assets/js/app.js.
 // Las capas visuales no interceptan Firebase, inventario ni cotizaciones.
 (() => {
-  const version = '20260719-private-admin-v18';
+  const version = '20260719-private-admin-v19';
 
   window.SD_WHATSAPP_NUMBER = window.SD_WHATSAPP_NUMBER || '50431517755';
 
